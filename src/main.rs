5a3b63b7a2b00ada109b5b2fda14b4a4aad2a122
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2024-03-08 20:34:39 +0100

    WOW, textures. OMG omfg hello

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 use num_complex::Complex;
 
 
-const ARRX:usize = 10;
+const ARRX:usize = 13;
 const ARRY:usize = 10;
 
 #[macroquad::main("Torus")]
@@ -19,19 +19,79 @@ async fn main() {
     }
 
 
+	let stone = Texture2D::from_file_with_format(
+	    include_bytes!("../textures/stone.png"),
+	    None,
+	);
+	let dirt = Texture2D::from_file_with_format(
+	    include_bytes!("../textures/dirt.png"),
+	    None,
+	);
+	let grass = Texture2D::from_file_with_format(
+	    include_bytes!("../textures/grass.png"),
+	    None,
+	);
+	stone.set_filter(FilterMode::Nearest);
+	dirt.set_filter(FilterMode::Nearest);
+	grass.set_filter(FilterMode::Nearest);
 
-
-    
     loop{
     	clear_background(BLACK);
+		
+    	
     	for x in 0..ARRX{
     		for y in 0..ARRY{
-    			let mut complex = Complex{re:array[x][y].y + array.len() as f32/2., im:array[x][y].x};
+    			let mut complex = Complex{re:array[x][y].y, im:array[x][y].x};
     			complex = r_p * Complex::exp(complex/r_p);
 				let node_x = complex.re;
 				let node_y = complex.im;
+				let size = f32::sqrt(f32::powf(node_x,2.)+f32::powf(node_y,2.))/1.65;
+
+
 				
-				draw_circle( node_x/50. + 500.0,  node_y/50. + 500.0, f32::sqrt(f32::powf(node_x,2.)+f32::powf(node_y,2.))/400., Color{r:0.1 * x as f32, g:0.1* y as f32, b:1.0, a:1.0});
+				//draw_circle( node_x/50. + 500.0,  node_y/50. + 500.0, size, Color{r:0.1 * x as f32, g:0.1* y as f32, b:1.0, a:1.0});
+				if y <= ARRY - 3{
+					draw_texture_ex(
+						&stone,
+						node_x - size/2. + 500.0,
+						node_y - size/2. + 500.0,
+						WHITE,
+						DrawTextureParams {
+							dest_size: Some(vec2(size,size)),
+							rotation: node_y.atan2(node_x),
+							..Default::default()
+						},
+					);	
+				}
+
+				if y == ARRY - 2{
+					draw_texture_ex(
+						&dirt,
+						node_x - size/2. + 500.0,
+						node_y - size/2. + 500.0,
+						WHITE,
+						DrawTextureParams {
+							dest_size: Some(vec2(size,size)),
+							rotation: node_y.atan2(node_x),
+							..Default::default()
+						},
+					);	
+				}
+				
+				if y == ARRY - 1{
+					draw_texture_ex(
+						&grass,
+						node_x - size/2. + 500.0,
+						node_y - size/2. + 500.0,
+						WHITE,
+						DrawTextureParams {
+							dest_size: Some(vec2(size,size)),
+							rotation: node_y.atan2(node_x)+std::f32::consts::PI/2.,
+							..Default::default()
+						},
+					);	
+				}
+				
     		}
     	}
 
