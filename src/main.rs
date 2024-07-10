c47d1fa1a507b9bbcb77f2001babdff89875ae91
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2024-07-10 22:56:45 +0200

    cmon cmon put the radio on

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use macroquad::prelude::*;
 use num_complex::Complex;
 
 
-const ARRX:usize = 100;
-const ARRY:usize = 100;
-const WORLD_SIZE:f32 = 100.0;
+const ARRX:usize = 3;
+const ARRY:usize = 10000;
+//const WORLD_SIZE:f32 = 100.0;
 
 
 #[macroquad::main("Torus")]
 async fn main() {
+	let mut playerx = 0.0;
+	let mut playery = 1.3;
+	let mut flydown = true;
+	let mut WORLD_SIZE:f32 = 100.0;
+
     println!("Hello, world!");
 
     let mut array:[[Vec2;ARRY];ARRX] = [[Vec2{x:0.0,y: 0.0};ARRY];ARRX];
@@ -17,7 +22,7 @@ async fn main() {
     	for y in 0..ARRY{
     		array[x][y] = Vec2{
 				x:(x as f32 *2.0 /ARRX as f32 -1.0) * std::f32::consts::PI,
-				y: y as f32 *2.0 /ARRY as f32 -1.0
+				y: (y as f32 - ARRY as f32)/5.0 //y as f32 *2.0 /ARRY as f32 -1.0
 			};
     	}
     }
@@ -42,7 +47,12 @@ async fn main() {
 
     loop{
     	clear_background(BLACK);
-		
+		let mut playercomplex = Complex{re:playery, im:playerx};
+		playercomplex = Complex::exp(playercomplex);
+		let player_node_x = playercomplex.re * 100.;
+		let player_node_y = playercomplex.im * 100.;
+		let player_size = f32::sqrt(f32::powf(playercomplex.re,2.)+f32::powf(playercomplex.im,2.))*(100.0*0.05);
+
     	
     	for x in 0..ARRX{
     		for y in 0..ARRY{
@@ -50,7 +60,7 @@ async fn main() {
     			complex = Complex::exp(complex);
 				let node_x = complex.re * WORLD_SIZE;
 				let node_y = complex.im * WORLD_SIZE;
-				let size = 8.0;//f32::sqrt(f32::powf(node_x,2.)+f32::powf(node_y,2.));
+				let size = f32::sqrt(f32::powf(complex.re,2.)+f32::powf(complex.im,2.))*(WORLD_SIZE*0.08);
 
 
 				
@@ -101,6 +111,15 @@ async fn main() {
 				
     		}
     	}
+		draw_circle(player_node_x - player_size/2. + 500.0,  player_node_y - player_size/2. + 500.0, player_size, Color{r:1., g:1. , b:1., a:1.0});
+		playerx +=0.1;
+		if flydown == true {playery -=0.01;}
+		else{playery +=0.01;}
+
+		if playery < -1.0 {flydown = false}
+		if playery > 1.0 {flydown = true}
+
+		WORLD_SIZE += WORLD_SIZE/10.;
 
     	next_frame().await
     }
