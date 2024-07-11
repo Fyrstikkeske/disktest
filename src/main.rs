551bcc2da6ac464e5d81e0bae2ea4ed9a293b451
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2024-07-12 01:16:56 +0200

    omg real? added a movable circle

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,21 @@ use macroquad::prelude::*;
 use num_complex::Complex;
 
 
-const ARRX:usize = 3;
-const ARRY:usize = 10000;
+const ARRX:usize = 70;
+const ARRY:usize = 100;
 //const WORLD_SIZE:f32 = 100.0;
 
 
 #[macroquad::main("Torus")]
 async fn main() {
 	let mut playerx = 0.0;
-	let mut playery = 1.3;
+	let mut playery = 0.;
 	let mut flydown = true;
-	let mut WORLD_SIZE:f32 = 100.0;
+	let mut WORLD_SIZE:f32 = 1000.0;
+	let mut world_offset_rotation:f32 = 0.0;
+	let mut world_offset_height:f32 = 0.0;
+	let mut world_offset_global_x:f32 = 960.0;
+	let mut world_offset_global_y:f32 = 540.0;
 
     println!("Hello, world!");
 
@@ -22,7 +26,7 @@ async fn main() {
     	for y in 0..ARRY{
     		array[x][y] = Vec2{
 				x:(x as f32 *2.0 /ARRX as f32 -1.0) * std::f32::consts::PI,
-				y: (y as f32 - ARRY as f32)/5.0 //y as f32 *2.0 /ARRY as f32 -1.0
+				y: (y as f32 - ARRY as f32)/12.0 //y as f32 *2.0 /ARRY as f32 -1.0
 			};
     	}
     }
@@ -47,20 +51,46 @@ async fn main() {
 
     loop{
     	clear_background(BLACK);
-		let mut playercomplex = Complex{re:playery, im:playerx};
+
+		if is_key_down(KeyCode::Right) {
+            world_offset_rotation -= 0.05;
+        }
+
+		if is_key_down(KeyCode::Left) {
+            world_offset_rotation += 0.05;
+        }
+
+		if is_key_down(KeyCode::Down) {
+            world_offset_height += 0.01;
+        }
+
+		if is_key_down(KeyCode::Up) {
+            world_offset_height -= 0.01;
+        }
+
+		if is_key_down(KeyCode::A) {
+            playerx -= 0.01;
+        }
+
+		if is_key_down(KeyCode::D) {
+            playerx += 0.01;
+        }
+
+
+		let mut playercomplex = Complex{re:playery + world_offset_height, im:playerx + world_offset_rotation};
 		playercomplex = Complex::exp(playercomplex);
-		let player_node_x = playercomplex.re * 100.;
-		let player_node_y = playercomplex.im * 100.;
-		let player_size = f32::sqrt(f32::powf(playercomplex.re,2.)+f32::powf(playercomplex.im,2.))*(100.0*0.05);
+		let player_node_x = playercomplex.re * WORLD_SIZE;
+		let player_node_y = playercomplex.im * WORLD_SIZE;
+		let player_size = 50.;//f32::sqrt(f32::powf(playercomplex.re,2.)+f32::powf(playercomplex.im,2.))*(100.0*0.05);
 
     	
     	for x in 0..ARRX{
     		for y in 0..ARRY{
-    			let mut complex = Complex{re:array[x][y].y, im:array[x][y].x};
-    			complex = Complex::exp(complex);
-				let node_x = complex.re * WORLD_SIZE;
-				let node_y = complex.im * WORLD_SIZE;
-				let size = f32::sqrt(f32::powf(complex.re,2.)+f32::powf(complex.im,2.))*(WORLD_SIZE*0.08);
+    			let mut worlds_complex = Complex{re:array[x][y].y + world_offset_height, im:array[x][y].x + world_offset_rotation};
+    			worlds_complex = Complex::exp(worlds_complex);
+				let node_x = worlds_complex.re * WORLD_SIZE;
+				let node_y = worlds_complex.im * WORLD_SIZE;
+				let size = f32::sqrt(f32::powf(worlds_complex.re,2.)+f32::powf(worlds_complex.im,2.))*(WORLD_SIZE*0.08);
 
 
 				
@@ -70,8 +100,8 @@ async fn main() {
 				if y <= ARRY - 3{
 					draw_texture_ex(
 						&stone,
-						node_x - size/2. + 500.0,
-						node_y - size/2. + 500.0,
+						node_x - size/2. + world_offset_global_x,
+						node_y - size/2. + world_offset_global_y,
 						WHITE,
 						DrawTextureParams {
 							dest_size: Some(vec2(size,size)),
@@ -84,8 +114,8 @@ async fn main() {
 				if y == ARRY - 2{
 					draw_texture_ex(
 						&dirt,
-						node_x - size/2. + 500.0,
-						node_y - size/2. + 500.0,
+						node_x - size/2. + world_offset_global_x,
+						node_y - size/2. + world_offset_global_y,
 						WHITE,
 						DrawTextureParams {
 							dest_size: Some(vec2(size,size)),
@@ -98,8 +128,8 @@ async fn main() {
 				if y == ARRY - 1{
 					draw_texture_ex(
 						&grass,
-						node_x - size/2. + 500.0,
-						node_y - size/2. + 500.0,
+						node_x - size/2. + world_offset_global_x,
+						node_y - size/2. + world_offset_global_y,
 						WHITE,
 						DrawTextureParams {
 							dest_size: Some(vec2(size,size)),
@@ -111,16 +141,14 @@ async fn main() {
 				
     		}
     	}
-		draw_circle(player_node_x - player_size/2. + 500.0,  player_node_y - player_size/2. + 500.0, player_size, Color{r:1., g:1. , b:1., a:1.0});
-		playerx +=0.1;
-		if flydown == true {playery -=0.01;}
-		else{playery +=0.01;}
+		draw_circle(player_node_x + world_offset_global_x,  player_node_y + world_offset_global_y, player_size, Color{r:1., g:1. , b:1., a:1.0});
+		//playerx +=0.1;
+		//if flydown == true {playery -=0.01;}
+		//else{playery +=0.01;}
 
-		if playery < -1.0 {flydown = false}
-		if playery > 1.0 {flydown = true}
-
-		WORLD_SIZE += WORLD_SIZE/10.;
-
+		//if playery < -1.0 {flydown = false}
+		//if playery > 1.0 {flydown = true}
+		
     	next_frame().await
     }
 }
