fbb79476777f22009a814de520254d68251d7cd4
Fyrstikkeske <79742888+Fyrstikkeske@users.noreply.github.com>
2024-07-12 22:59:12 +0200

    RAAAAAH

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 use num_complex::Complex;
 
 
-const ARRX:usize = 5000;
+const ARRX:usize = 20;
 const ARRY:usize = 3;
 //const WORLD_SIZE:f32 = 100.0;
 
@@ -14,7 +14,7 @@ async fn main() {
 	let mut flydown = true;
 	let mut WORLD_SIZE:f32 = 1.0;
 	let mut world_offset_rotation:f32 = 0.0;
-	let mut world_offset_height:f32 = 7.0;
+	let mut world_offset_height:f32 = 6.0;
 	let mut world_offset_global_x:f32 = 960.0;
 	let mut world_offset_global_y:f32 = 540.0;
 
@@ -26,7 +26,7 @@ async fn main() {
     	for y in 0..ARRY{
     		box_world[x][y] = Vec2{
 				x:(x as f32 *2.0 /ARRX as f32 -1.0) * std::f32::consts::PI,
-				y: (y as f32 - ARRY as f32)  //12.0 //y as f32 *2.0 /ARRY as f32 -1.0
+				y: ((y+1) as f32 - ARRY as f32) / 1.00 //y as f32 *2.0 /ARRY as f32 -1.0
 			};
     	}
     }
@@ -61,11 +61,11 @@ async fn main() {
         }
 
 		if is_key_down(KeyCode::Down) {
-            world_offset_height += 0.04;
+            world_offset_height += 0.01;
         }
 
 		if is_key_down(KeyCode::Up) {
-            world_offset_height -= 0.04;
+            world_offset_height -= 0.01;
         }
 
 		if is_key_down(KeyCode::A) {
@@ -98,9 +98,9 @@ async fn main() {
     			worlds_complex = Complex::exp(worlds_complex);
 				let node_x = worlds_complex.re;
 				let node_y = worlds_complex.im;
-				let size = f32::sqrt(f32::powf(worlds_complex.re,2.)+f32::powf(worlds_complex.im,2.))/ ((0.16 * ARRX as f32)+0.373);
+				let size = f32::sqrt(f32::powf(worlds_complex.re,2.)+f32::powf(worlds_complex.im,2.)) *((std::f32::consts::PI*2.)/ARRX as f32);
 
-
+				
 				
 				//not needed but here as why not in case 
 				//draw_circle( node_x/50. + 500.0,  node_y/50. + 500.0, size, Color{r:0.1 * x as f32, g:0.1* y as f32, b:1.0, a:1.0});
